3f2a1faaa68c78eea27aa98de95675dedb2432be
Aniket roy <royaniket20@gmail.com>
2023-09-18 16:22:49 +0530

    added describe method

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -7,82 +7,70 @@ const {
   DB_SERVER_URL,
 } = require("../../Appservices/mongoConfig.js");
 const { loadPlanetDataSimple } = require("../../models/planets.model.js");
-
-beforeAll(async () => {
-  await connectToMongo();
-  await loadPlanetDataSimple(); //Can only be called from Async Function
-  console.log(`TEST CASE WILL BE RUNNING ON DB - ${DB_SERVER_URL}`);
-
-});
-
-afterAll(async () => {
-  await disconnectFromMongo();
-});
-
-//No need to Import Jest know about this packages
-test("Test GET /launches : It shoud respond with 200 status code", async () => {
-  //Write actual test case here
-  const response = await request(app)
-    .get("/launches")
-    .set("Origin", "http://localhost:8000"); //Setting Header
-  //const response = 200;
-  expect(response.status).toBe(200);
-
-  //We can do alternative way also Directly checking via supertest chain
-  request(app)
-    .get("/launches")
-    .set("Origin", "http://localhost:8000")
-    .expect(200); //Setting Header
-});
-
-test("Test POST /launches : It shoud respond with 201 status code", async () => {
-  //We can do alternative way also Directly checking via supertest chain
-  const response = await request(app)
-    .post("/launches")
-    .set("Origin", "http://localhost:8000")
-    .send({
-      launchDate: "2023-09-10",
-      mission: "Kepler Exploration X",
-      rocket: "Explorer IS1",
-      target: "Kepler-1652 b",
-    });
-  console.log(response.text);
-  expect(response.status).toBe(201);
-});
-
-test("Test POST /launches : It shoud respond with 201 status code yload validation", async () => {
-  //We can do alternative way also Directly checking via supertest chain
-  let resp = await request(app)
-    .post("/launches")
-    .set("Origin", "http://localhost:8000")
-    .send({
-      launchDate: "2023-09-10",
-      mission: "Kepler Exploration X",
-      rocket: "Explorer IS1",
-      target: "Kepler-1652 b",
-    })
-    .expect(201);
-
-  expect(resp.body).toMatchObject({
-    mission: "Kepler Exploration X",
-    rocket: "Explorer IS1",
-    target: "Kepler-1652 b",
+describe("Launches API", () => {
+  //************************************************ */
+  beforeAll(async () => {
+    await connectToMongo();
+    await loadPlanetDataSimple(); //Can only be called from Async Function
+    console.log(`TEST CASE WILL BE RUNNING ON DB - ${DB_SERVER_URL}`);
   });
-});
+  //************************************************ */
+  afterAll(async () => {
+    await disconnectFromMongo();
+  });
+  //************************************************ */
+  //No need to Import Jest know about this packages
+  describe("Test GET /launches", () => {
+    test("Test GET /launches : It shoud respond with 200 status code", async () => {
+      //Write actual test case here
+      const response = await request(app)
+        .get("/launches")
+        .set("Origin", "http://localhost:8000"); //Setting Header
+      //const response = 200;
+      expect(response.status).toBe(200);
 
-test("Test POST /launches : It shoud respond with 400 status code", async () => {
-  //We can do alternative way also Directly checking via supertest chain
-  const response = await request(app)
-    .post("/launches")
-    .set("Origin", "http://localhost:8000")
-    .send({
-      mission: "Kepler Exploration X",
-      rocket: "Explorer IS1",
-      target: "Kepler-1652 b",
-    })
-    .expect(400);
-  console.log(response.text);
-  expect(response.text).toStrictEqual(
-    '{"error":"Missing something in payload","actualRequest":{"mission":"Kepler Exploration X","rocket":"Explorer IS1","target":"Kepler-1652 b"}}'
-  );
+      //We can do alternative way also Directly checking via supertest chain
+      // request(app)
+      //   .get("/launches")
+      //   .set("Origin", "http://localhost:8000")
+      //   .expect(200); //Setting Header
+    });
+  });
+  //************************************************ */
+  describe("Test POST /launch", () => {
+    test("Test POST /launches : It shoud respond with 201 status code", async () => {
+      //We can do alternative way also Directly checking via supertest chain
+      const response = await request(app)
+        .post("/launches")
+        .set("Origin", "http://localhost:8000")
+        .send({
+          launchDate: "2023-09-10",
+          mission: "Kepler Exploration X",
+          rocket: "Explorer IS1",
+          target: "Kepler-1652 b",
+        });
+      console.log(response.text);
+      expect(response.status).toBe(201);
+    });
+
+    //----------------------------------------------------------
+
+    test("Test POST /launches : It shoud respond with 400 status code", async () => {
+      //We can do alternative way also Directly checking via supertest chain
+      const response = await request(app)
+        .post("/launches")
+        .set("Origin", "http://localhost:8000")
+        .send({
+          mission: "Kepler Exploration X",
+          rocket: "Explorer IS1",
+          target: "Kepler-1652 b",
+        })
+        .expect(400);
+      console.log(response.text);
+      expect(response.text).toStrictEqual(
+        '{"error":"Missing something in payload","actualRequest":{"mission":"Kepler Exploration X","rocket":"Explorer IS1","target":"Kepler-1652 b"}}'
+      );
+    });
+    //--------------------------------------------------------------
+  });
 });
